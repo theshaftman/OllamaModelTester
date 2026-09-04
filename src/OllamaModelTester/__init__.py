ff2a3fb70a40b352a99f4d87e273693ea6cb23b2
theshaftman <mariyan.apostolov89@gmail.com>
2026-09-04 12:48:42 +0300

    v-0-0-4 Init

--- src/OllamaModelTester/__init__.py
+++ src/OllamaModelTester/__init__.py
@@ -1,5 +1,5 @@
 from .model.ollama_model_tester import OllamaModelTester
 from .model.model_visualizer import ModelVisualizer
 
-__vesion__ = '0.0.3'
+__vesion__ = '0.0.4'
 __all__ = ['OllamaModelTester', 'ModelVisualizer']
